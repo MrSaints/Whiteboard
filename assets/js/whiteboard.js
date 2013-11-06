8d639bf3f93963e212b61ef7e5592ee3b2a1990e
MrSaints <ian@fyianlai.com>
2013-11-06 18:48:51 +0000

    Added sending/restoring draw data

--- assets/js/whiteboard.js
+++ assets/js/whiteboard.js
@@ -1,3 +1,18 @@
-var whiteboard = new DrawingBoard.Board('js-whiteboard', {
+var $whiteboard = new DrawingBoard.Board('js-whiteboard', {
 	controls: false
+});
+
+$whiteboard.ev.bind('board:drawing', function ($data) {
+    //console.log($data);
+});
+
+TogetherJS.hub.on("togetherjs.hello", function () {
+    TogetherJS.send({
+        type: "init",
+        image: $whiteboard.getImg()
+    });
+});
+
+TogetherJS.hub.on("init", function (msg) {
+    $whiteboard.setImg(msg.image);
 });
